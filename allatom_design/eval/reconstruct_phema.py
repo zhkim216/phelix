e8c520f9eb6f0258827a8910d098c18d7169ab61
Richard <richardshuai98@gmail.com>
2024-09-28 22:04:02 -0700

    first pass at post hoc ema reconstruction

---
+++ allatom_design/eval/reconstruct_phema.py
@@ -0,0 +1,111 @@
+import copy
+import glob
+import re
+import shutil
+from pathlib import Path
+
+import hydra
+import lightning as L
+import torch
+import yaml
+from natsort import natsorted
+from omegaconf import DictConfig, OmegaConf
+
+import allatom_design.model.phema as phema
+from tqdm import tqdm
+from allatom_design.model.atom_denoiser.lit_ad_model import LitAtomDenoiser
+
+
+@hydra.main(config_path="../configs/eval", config_name="reconstruct_phema", version_base="1.3.2")
+def main(cfg: DictConfig):
+    """
+    Script for running EDM2's post hoc EMA.
+    """
+    cfg_dict = OmegaConf.to_container(cfg, resolve=True)
+
+    # Set seeds
+    L.seed_everything(cfg.seed)
+    torch.backends.cudnn.deterministic = True  # nonrandom CUDNN convolution algo, maybe slower
+    torch.backends.cudnn.benchmark = False  # nonrandom selection of CUDNN convolution, maybe slower
+
+    # Set up models (in eval mode)
+    torch.set_grad_enabled(False)
+
+    # Create out dirs and preserve config
+    if cfg.out_dir is None:
+        cfg.out_dir = f"{cfg.denoiser_train_dir}/post_hoc_ema_ckpts"
+
+    if cfg.overwrite_out_dir and Path(cfg.out_dir).exists():
+        # delete existing out_dir
+        print(f"Deleting pre-existing out_dir: {cfg.out_dir}")
+        shutil.rmtree(cfg.out_dir)
+
+    Path(cfg.out_dir).mkdir(parents=True, exist_ok=True)
+    with open(Path(cfg.out_dir, "config.yaml"), "w") as f:
+        yaml.safe_dump(cfg_dict, f)
+
+    # Gather atom denoiser checkpoints (TODO: make this also support a sequence denoiser)
+    pattern = re.compile(r"ad-epoch\d+\.ckpt$")  # only consider ckpts of form ad-epochXXXX.ckpt
+    ad_ckpts = glob.glob(f"{cfg.denoiser_train_dir}/checkpoints/*.ckpt")
+    ad_ckpts = natsorted([ckpt for ckpt in ad_ckpts if pattern.search(Path(ckpt).name)])
+
+    # Compute posthoc EMA coefficients based on input steps/stds and desired output step/stds
+    out_step = cfg.out_step
+    out_stds = cfg.out_stds
+
+    in_steps = []
+    in_stds = []
+    in_ckpts = []
+    for ckpt in tqdm(ad_ckpts, desc="Loading checkpoints for computing phema coefficients"):
+        ckpt_dict = torch.load(ckpt, map_location="cpu")
+        if ckpt_dict["global_step"] > out_step:
+            # only consider ckpts up to the desired output step
+            break
+
+        stds = ckpt_dict["ema_tracker"]["stds"]
+        in_stds += stds
+        in_steps += [ckpt_dict["global_step"]] * len(stds)
+        in_ckpts.append(ckpt)
+
+    coefs = phema.solve_posthoc_coefficients(in_steps, in_stds, out_step, out_stds)
+
+    # Initialize output state dicts
+    out_state_dicts = []
+    for _ in out_stds:
+        out_state_dict = copy.deepcopy(ckpt_dict["ema_tracker"]["emas"][0])
+        for p in out_state_dict.values():
+            p.data.zero_()
+        out_state_dicts.append(out_state_dict)
+
+    # Compute EMAs
+    ei = 0
+    for ckpt in tqdm(in_ckpts, desc="Computing EMA weights"):
+        ckpt_dict = torch.load(ckpt, map_location="cpu")
+        for i in range(len(ckpt_dict["ema_tracker"]["emas"])):
+            ema_weights_in = ckpt_dict["ema_tracker"]["emas"][i]
+
+            # Update output state dicts
+            for j, out_state_dict in enumerate(out_state_dicts):
+                for n, p in out_state_dict.items():
+                    p += coefs[ei, j] * ema_weights_in[n]
+            ei += 1
+
+    for out_std, out_state_dict in zip(out_stds, out_state_dicts):
+        # Create new checkpoints containing EMA weights
+        out_ckpt = Path(cfg.out_dir, f"ad-ema-step{out_step}-std{out_std:.2f}.ckpt")
+        base_ckpt = ad_ckpts[-1]  # besides the updated EMA weights, the rest of the model will be the same as the last checkpoint
+        ckpt_dict = torch.load(base_ckpt, map_location="cpu")
+        lit_ad_model = LitAtomDenoiser.load_from_checkpoint(base_ckpt)
+
+        model_state_dict = lit_ad_model.model.state_dict()
+        model_state_dict.update(out_state_dict)
+        _, unexpected_keys = lit_ad_model.model.load_state_dict(model_state_dict, strict=False)
+        if unexpected_keys:
+            raise ValueError(f"Unexpected keys in EMA state dict: {unexpected_keys}")
+
+        ckpt_dict["state_dict"] = lit_ad_model.state_dict()
+        torch.save(ckpt_dict, out_ckpt)
+
+
+if __name__ == "__main__":
+    main()
