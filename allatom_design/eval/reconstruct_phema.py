68a71d939afcd90d9de147884b1c8e7a061289f2
Richard <richardshuai98@gmail.com>
2024-10-04 21:37:16 -0700

    add arange option for phema; test on a10_aasd3 seq denoiser

--- allatom_design/eval/reconstruct_phema.py
+++ allatom_design/eval/reconstruct_phema.py
@@ -15,6 +15,7 @@ import allatom_design.model.phema as phema
 from tqdm import tqdm
 from allatom_design.model.atom_denoiser.lit_ad_model import LitAtomDenoiser
 from allatom_design.model.seq_denoiser.lit_sd_model import LitSeqDenoiser
+import numpy as np
 
 
 @hydra.main(config_path="../configs/eval", config_name="reconstruct_phema", version_base="1.3.2")
@@ -55,7 +56,11 @@ def main(cfg: DictConfig):
 
     # Compute posthoc EMA coefficients based on input steps/stds and desired output step/stds
     out_step = cfg.out_step
-    out_stds = cfg.out_stds
+    if cfg.out_stds is None:
+        out_stds = np.arange(*cfg.out_stds_arange)
+    else:
+        out_stds = cfg.out_stds
+    print(f"Computing posthoc EMA coefficients for: \nStep {out_step} \nStds {out_stds}...")
 
     in_steps = []
     in_stds = []
@@ -96,7 +101,7 @@ def main(cfg: DictConfig):
 
     for out_std, out_state_dict in zip(out_stds, out_state_dicts):
         # Create new checkpoints containing EMA weights
-        out_ckpt = Path(cfg.out_dir, f"ema-step{out_step}-std{out_std:.2f}.ckpt")
+        out_ckpt = Path(cfg.out_dir, f"ema-step{out_step}-std{out_std:.3f}.ckpt")
         base_ckpt = denoiser_ckpts[0]  # besides the updated EMA weights, the rest of the model will be the same as the last checkpoint
         ckpt_dict = torch.load(base_ckpt, map_location="cpu")
 
