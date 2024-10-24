940300c8d8321736c5c1dc4c3dfd63b0085dd102
Richard <richardshuai98@gmail.com>
2024-10-23 20:05:37 -0700

    fix corrector steps in inv fold script

--- allatom_design/eval/eval_inv_fold_training.py
+++ allatom_design/eval/eval_inv_fold_training.py
@@ -259,6 +259,7 @@ def main(cfg: DictConfig):
                         timesteps=timesteps,
                         aatype_decoding_order_mode=cfg.aatype_decoding_order_mode,
                         num_corrector_steps=cfg.num_corrector_steps,
+                        corrector_step_ratio=cfg.corrector_step_ratio,
                         cond_labels=cond_labels_in,
                         scd_inputs=scd_inputs,
                     )
