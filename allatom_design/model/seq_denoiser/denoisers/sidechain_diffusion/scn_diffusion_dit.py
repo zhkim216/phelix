b5_aasd4: fix scn rollout rmsd calculation; only compute confidence on newly unmasked sidechains

allatom_design/model/seq_denoiser/denoisers/sidechain_diffusion/scn_diffusion_dit.py
@@ -186,10 +186,13 @@ class SidechainDiffusionModule(nn.Module):
                                   "autoguidance_cfg": dict(conf_cfg.scn_diffusion.autoguidance_cfg),
                                   }
 
-                    rollout_aux_inputs = {"seq_mlm_mask": scd_mlm_mask_batched[::M],  # we pass in future residue mask, but without batch multiplier
-                                          "scd": scd_inputs}
+                    # Randomly choose some residues to unmask for rollout
+                    scd_mlm_mask_rollout = self.unmask_future_residues(aux_inputs["seq_mlm_mask"], seq_mask)
+                    x_scn_gt_rollout = x_scn_gt * rearrange(scd_mlm_mask_rollout, "b n -> b n 1 1")
 
                     # Run diffusion mini rollout
+                    rollout_aux_inputs = {"seq_mlm_mask": scd_mlm_mask_rollout,
+                                          "scd": scd_inputs}
                     x1_scn_rollout, _ = self.sidechain_diffusion(h_V, aatype, x_bb,
                                                                  seq_mask, residue_index,
                                                                  aux_inputs=rollout_aux_inputs,
@@ -206,14 +209,14 @@ class SidechainDiffusionModule(nn.Module):
                                                seq_mask.detach(),
                                                residue_index.detach(),
                                                chain_index=torch.zeros_like(residue_index),
-                                               scd_mlm_mask=scd_mlm_mask_batched[::M].detach(),
+                                               scd_mlm_mask=scd_mlm_mask_rollout.detach(),
                                                )  # TODO: pass in chain index
                 diffusion_aux["confidence_aux"] = {
                     "psce_logits": psce_logits,
                     "sce_bins_cfg": self.confidence_module.sce_bins_cfg,
                     "scn_pred_rollout": x1_scn_rollout,
-                    "scn_target": x_scn_gt_batched[::M],
-                    "scd_mlm_mask": scd_mlm_mask_batched[::M],
+                    "scn_target": x_scn_gt_rollout,
+                    "scd_mlm_mask": scd_mlm_mask_rollout,
                 }
 
             # Outputs

allatom_design/model/seq_denoiser/sd_loss.py
@@ -126,18 +126,21 @@ class SDLoss(nn.Module):
                 scd_mlm_mask = confidence_outputs["scd_mlm_mask"]
                 scn_atom_mask = batch["atom_mask"][..., rc.non_bb_idxs]
 
-                # mask out loss when masking aatype
-                mask = scn_atom_mask * rearrange(scd_mlm_mask, "b n -> b n 1")
+                # Construct residue-level mask
+                new_scn_mask = scd_mlm_mask * (1 - outputs["seq_mlm_mask"])  # only compute loss over newly unmasked sidechains
+                new_scn_mask = new_scn_mask * (1 - batch["seq_unk_mask"])  # mask out true unk tokens
+                assert ((1 - scd_mlm_mask) * outputs["seq_mlm_mask"]).sum() == 0  # scd_mlm_mask should always be 1 if seq_mlm_mask is 1
 
                 # Compute PSCE confidence loss
-                aux["psce_loss"] = psce_loss(psce_logits, scn_pred_rollout, scn_target, mask,
+                psce_mask = scn_atom_mask * rearrange(new_scn_mask, "b n -> b n 1")  # mask out ghost and missing sidechain atoms
+                aux["psce_loss"] = psce_loss(psce_logits, scn_pred_rollout, scn_target, psce_mask,
                                              self.cfg.inf,
                                              **confidence_outputs["sce_bins_cfg"])
 
-                # monitor rollout sidechain RMSD (averaged across residues)
-                msd_per_pos = (mask[..., None] * (scn_target - scn_pred_rollout)).pow(2).sum(dim=(-1, -2)) / mask.sum(dim=-1).clamp(min=1)
-                rmsd_per_pos = msd_per_pos.sqrt()
-                rmsd = (rmsd_per_pos * batch["seq_mask"]).sum(dim=-1) / batch["seq_mask"].sum(dim=-1).clamp(min=1)
+                # monitor rollout sidechain RMSD (averaged across residues with newly unmasked sidechains)
+                msd_per_res = (psce_mask[..., None] * (scn_target - scn_pred_rollout)).pow(2).sum(dim=(-1, -2)) / psce_mask.sum(dim=-1).clamp(min=1)
+                rmsd_per_res = msd_per_res.sqrt()
+                rmsd = (rmsd_per_res * new_scn_mask).sum(dim=-1) / new_scn_mask.sum(dim=-1).clamp(min=1)
                 aux_monitor["rollout/scn_rmsd"] = rmsd.mean().detach().clone()
 
 