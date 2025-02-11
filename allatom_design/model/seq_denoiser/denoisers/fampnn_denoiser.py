df948acde3f52ee769e4d565219abf6ae0d2bae4
Richard <richardshuai98@gmail.com>
2025-02-11 11:03:50 -0800

    c3_aasd7: c3_aasd5 but with last layer embeddings as well

--- allatom_design/model/seq_denoiser/denoisers/fampnn_denoiser.py
+++ allatom_design/model/seq_denoiser/denoisers/fampnn_denoiser.py
@@ -46,7 +46,7 @@ class FAMPNNDenoiser(BaseSeqDenoiser):
             for param in self.esmc.parameters():
                 param.requires_grad = False
 
-            self.esmc_combine = nn.Parameter(torch.zeros(ESMC_INFO[self.esmc_name]["n_layer"]))
+            self.esmc_combine = nn.Parameter(torch.zeros(ESMC_INFO[self.esmc_name]["n_layer"] + 1))  # hidden states + last layer embeddings
             self.esmc_mlp = nn.Sequential(
                 nn.LayerNorm(ESMC_INFO[self.esmc_name]["n_channel"]),
                 nn.Linear(ESMC_INFO[self.esmc_name]["n_channel"], cfg.fampnn.n_channel),
@@ -114,7 +114,8 @@ class FAMPNNDenoiser(BaseSeqDenoiser):
             # make sure to apply mask
             with torch.no_grad():
                 protein_tensor = self.af2_to_esmc(aatype_noised, seq_mask)
-                esmc_embed = self.esmc.logits(protein_tensor, LogitsConfig(sequence=True, return_embeddings=True, return_hidden_states=True)).hidden_states
+                logits_output = self.esmc.logits(protein_tensor, LogitsConfig(sequence=True, return_embeddings=True, return_hidden_states=True))
+                esmc_embed = torch.cat([logits_output.hidden_states, logits_output.embeddings.unsqueeze(0)], dim=0)
                 esmc_embed = esmc_embed[:, :, 1:-1]  # remove CLS (first) and EOS (last, but possibly pad)
 
                 # preprocess ESM sequence embedding
