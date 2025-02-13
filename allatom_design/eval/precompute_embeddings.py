1c902d2a7f57470294c9047822fa11f838704c18
Richard <richardshuai98@gmail.com>
2025-02-13 11:28:15 -0800

    precompute mpnn embeddings too

--- allatom_design/eval/precompute_embeddings.py
+++ allatom_design/eval/precompute_embeddings.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from pathlib import Path
 
 import hydra
+import lightning as L
 import numpy as np
 import pandas as pd
 import torch
@@ -22,35 +23,55 @@ def main(cfg: DictConfig):
     device = torch.device("cuda")
     Path(cfg.out_dir).mkdir(parents=True, exist_ok=True)
 
+    # Set seeds
+    L.seed_everything(cfg.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
     # Get pdb keys for each phase
-    phase_to_pdb_key_files = {"train": cfg.train_pdb_key_file, "eval": cfg.eval_pdb_key_file, "eval2": cfg.eval2_pdb_key_file}
-    pdbs_df = defaultdict(list)
-    for phase, file_path in phase_to_pdb_key_files.items():
-        if file_path is None:
-            # skip if key file is not provided
-            continue
+    if not cfg.use_precomputed_key_files:
+        # Subset from pdb key files
+        phase_to_pdb_key_files = {"train": cfg.train_pdb_key_file, "eval": cfg.eval_pdb_key_file, "eval2": cfg.eval2_pdb_key_file}
+        pdbs_df = defaultdict(list)
+        for phase, file_path in phase_to_pdb_key_files.items():
+            if file_path is None:
+                # skip if key file is not provided
+                continue
 
-        with open(file_path) as f:
-            phase_pdbs = np.array(f.read().split("\n")[:-1])
+            with open(file_path) as f:
+                phase_pdbs = np.array(f.read().split("\n"))
 
-        if cfg.subsample_n is not None:
-            # subsample to at max subsample_n pdbs from each phase
-            phase_pdbs = np.random.choice(phase_pdbs, min(cfg.subsample_n, len(phase_pdbs)), replace=False)
+            if cfg.subsample_n is not None:
+                # subsample to at max subsample_n pdbs from each phase
+                phase_pdbs = np.random.choice(phase_pdbs, min(cfg.subsample_n, len(phase_pdbs)), replace=False)
 
-        pdb_keys = [Path(pdb).stem for pdb in phase_pdbs]  # remove .pdb extension in case it exists in key list
-        pdbs_df["pdb_key"].extend(pdb_keys)
-        pdbs_df["phase"].extend([phase] * len(pdb_keys))
-    pdbs_df = pd.DataFrame(pdbs_df)
+            pdb_keys = [Path(pdb).stem for pdb in phase_pdbs]  # remove .pdb extension in case it exists in key list
+            pdbs_df["pdb_key"].extend(pdb_keys)
+            pdbs_df["phase"].extend([phase] * len(pdb_keys))
+        pdbs_df = pd.DataFrame(pdbs_df)
 
-    # Dump pdbs with pre-computed embeddings to keys lists
-    for phase in pdbs_df["phase"].unique():
-        phase_pdbs = pdbs_df[pdbs_df["phase"] == phase]
-        with open(Path(cfg.out_dir, f"precomputed_{phase}_pdb_keys.list"), "w") as f:
-            f.write("\n".join(phase_pdbs["pdb_key"]))
+        # Dump pdbs with pre-computed embeddings to keys lists
+        for phase in pdbs_df["phase"].unique():
+            phase_pdbs_df = pdbs_df[pdbs_df["phase"] == phase]
+            phase_pdbs_df["pdb_key"].to_csv(Path(cfg.out_dir, f"precomputed_{phase}_pdb_keys.csv"), index=False, header=False)
+
+        all_pdb_keys = pdbs_df["pdb_key"].values
+    else:
+        # Load precomputed pdb keys
+        all_pdb_keys = []
+        phases = ["train", "eval", "eval2"]
+        precomputed_key_files = [Path(cfg.out_dir, f"precomputed_{phase}_pdb_keys.csv") for phase in phases]
+        for phase, file_path in zip(phases, precomputed_key_files):
+            if not file_path.exists():
+                print(f"Skipping {phase} phase: precomputed key file not found")
+                continue
+
+            phase_pdbs = pd.read_csv(file_path, header=None).values.flatten()
+            all_pdb_keys.extend(phase_pdbs)
 
     # Compute embeddings for FPD
     pdbs_dir = Path(cfg.pdbs_dir)
-    pdb_paths = [str(pdbs_dir / f"{pdb}.pdb") for pdb in pdbs_df["pdb_key"]]
+    pdb_paths = [str(pdbs_dir / f"{pdb}.pdb") for pdb in all_pdb_keys]
 
     if cfg.compute_mpnn:
         mpnn_cfg = OmegaConf.load(cfg.mpnn.mpnn_cfg)
