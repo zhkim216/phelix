dc64d1a446358a486d80795585ead2992e7618b2
Richard <richardshuai98@gmail.com>
2025-02-28 14:41:17 -0800

    add sc dataset parsing

---
+++ allatom_design/data/preprocessing/af3/parse_sc_dataset_csv.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Parse the self_consistency_metrics.csv output from construct_sc_dataset.py to get FAMPNN1 designability statistics at T=0.1.
+"""
+import argparse
+from pathlib import Path
+
+import pandas as pd
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse self_consistency_metrics csv")
+    parser.add_argument("--train_sc_csv", type=str, help="Path to self_consistency_metrics csv.")
+    parser.add_argument("--eval_sc_csv", type=str, help="Path to self_consistency_metrics csv.")
+    parser.add_argument("--out_dir", type=str, help="Output directory.")
+    args = parser.parse_args()
+
+    # First, combine train and eval csvs
+    sc_df = pd.concat([pd.read_csv(args.train_sc_csv), pd.read_csv(args.eval_sc_csv)], ignore_index=True)
+
+    # Filter for T=0.1, take only the first entry for each pdb_key
+    sc_df = sc_df[sc_df["temperature"] == 0.1].drop_duplicates(subset=["pdb_key"], keep="first").reset_index(drop=True)
+    df_out = sc_df[["pdb_key", "temperature", "sc_ca_rmsd", "sc_aa_rmsd", "sc_ca_tm", "avg_plddt"]]
+
+    # Additional info
+    df_out["sample_name"] = sc_df["pdb_name"]  # in case we want to know which FAMPNN sample it is
+    df_out["seq_length"] = sc_df["pred_seq"].str.len()
+
+    # Save to out directory
+    Path(args.out_dir).mkdir(parents=True, exist_ok=True)
+    df_out.to_csv(f"{args.out_dir}/designability_stats.csv", index=False, header=True)
+
+
+if __name__ == "__main__":
+    main()
