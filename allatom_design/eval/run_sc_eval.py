0161875c273d6f9dcd28c2b9bccb6ff945073fdd
Richard <richardshuai98@gmail.com>
2025-03-18 15:25:45 -0700

    run sc eval for proteinmpnn on af3 pdb monomer

--- allatom_design/eval/run_sc_eval.py
+++ allatom_design/eval/run_sc_eval.py
@@ -13,13 +13,16 @@ from omegaconf import DictConfig, OmegaConf
 
 from allatom_design.eval import eval_metrics
 from allatom_design.eval.eval_setup_utils import get_pdb_files, wandb_setup
+from allatom_design.eval.fampnn_utils import get_seq_des_model
 from allatom_design.eval.folding_utils import get_struct_pred_model
 
 
-@hydra.main(config_path="../configs/eval", config_name="eval_seq_des_training", version_base="1.3.2")
+@hydra.main(config_path="../configs/eval", config_name="run_sc_eval", version_base="1.3.2")
 def main(cfg: DictConfig):
     """
-    Simple script for running self-consistency eval on a set of PDBs using the sequence in the PDBs.
+    Simple script for running self-consistency eval on a set of PDBs and logging to wandb.
+    If use_seq_design is False, we will evaluate using the sequence in the PDB.
+    If use_seq_design is True, we will evaluate using a sequence designed by seq_des_cfg.model_name ("proteinmpnn" or "fampnn")
     """
     cfg_dict = OmegaConf.to_container(cfg, resolve=True)
 
@@ -44,12 +47,19 @@ def main(cfg: DictConfig):
     ### Load in PDB files to eval on ###
     pdb_files = get_pdb_files(**cfg.input_cfg)
 
-    # Load in structure prediction model for co-design self-consistency evals
+    # Load in sequence design model
+    if cfg.use_seq_design:
+        seq_des_model = get_seq_des_model(cfg.seq_des_cfg, device=device)
+    else:
+        seq_des_model = None  # use sequence found in PDB
+
+    # Load in structure prediction model
     struct_pred_model = get_struct_pred_model(cfg.struct_pred_cfg, device=device)
 
+    # Run self-consistency evaluation
     sc_info = eval_metrics.run_self_consistency_eval(
         pdb_files,
-        None,
+        seq_des_model,
         struct_pred_model,
         device,
         out_dir=log_dir,
@@ -63,8 +73,8 @@ def main(cfg: DictConfig):
             sc_metrics[f"{k}"].append(v.item())
 
     # Update metrics
-    out_metrics = {f"seq_des/mean/{k}": np.mean(v) for k, v in sc_metrics.items()}
-    out_metrics.update({f"seq_des/median/{k}": np.median(v) for k, v in sc_metrics.items()})
+    out_metrics = {f"mean/{k}": np.mean(v) for k, v in sc_metrics.items()}
+    out_metrics.update({f"median/{k}": np.median(v) for k, v in sc_metrics.items()})
 
     # Dump to output directory
     with open(os.path.join(log_dir, "metrics.pkl"), "wb") as f:
