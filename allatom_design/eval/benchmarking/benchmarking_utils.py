326e6bd6ac286a8b0393f0c79a1b45b7f19317ac
Richard <richardshuai98@gmail.com>
2025-05-21 19:11:42 -0700

    set up chroma evals

--- allatom_design/eval/benchmarking/benchmarking_utils.py
+++ allatom_design/eval/benchmarking/benchmarking_utils.py
@@ -8,13 +8,22 @@ import allatom_design.data.const as const
 
 
 def thread_sequence_onto_example(example: dict[str, TensorType["1 n ..."]],
-                                 res_type_one_hot: TensorType["n c", int],
-                                 label_seq_id: TensorType["n", int],
-                                 ) -> dict[str, TensorType["1 n ..."]]:
+                                 new_res_type: TensorType["n2 c", int],
+                                 label_seq_id: TensorType["n2", int],
+                                 mask: TensorType["n2", bool] | None = None) -> dict[str, TensorType["1 n ..."]]:
     """
     Thread a sequence onto an example. Returns a deepcopy of the example.
+
+    - mask: if provided, only update residues where mask is True
     """
     example = copy.deepcopy(example)
+
+    # Handle mask so we only update where mask is True
+    if mask is None:
+        mask = torch.ones_like(label_seq_id, dtype=torch.bool)
+
+    label_seq_id = label_seq_id[mask]
+    new_res_type = new_res_type[mask]
 
     # For now, make sure there is only one protein chain
     protein_mask = example["mol_type"] == const.chain_type_ids["PROTEIN"]
@@ -22,11 +31,12 @@ def thread_sequence_onto_example(example: dict[str, TensorType["1 n ..."]],
     if n_prot_chains > 1:
         raise ValueError(f"Found {n_prot_chains} protein chains in {example['record_id']}. For now, we only support threading sequences onto single-chain proteins.")
 
-    # Set all protein residues to X and erase all sidechain coordinates
+    # Set all missing protein residues to X and erase all sidechain coordinates
     protein_res_type = torch.full_like(protein_mask, const.token_ids["UNK"], dtype=torch.long)
     protein_res_type = F.one_hot(protein_res_type, num_classes=example["res_type"].shape[-1])  # [1, n_protein, 33]
     protein_res_type = protein_res_type.squeeze(0)  # temporarily squeeze out batch dimension
-    protein_res_type[label_seq_id - 1] = res_type_one_hot  # label_seq_id is 1-indexed
+    protein_res_type[label_seq_id - 1] = new_res_type  # label_seq_id is 1-indexed
+
     example["res_type"][protein_mask] = protein_res_type.unsqueeze(0)
     example["coords"][example["prot_scn_atom_mask"].bool()] = 0.0
 
