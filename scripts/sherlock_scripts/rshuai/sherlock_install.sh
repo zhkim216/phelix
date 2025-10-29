e0b0d8d275c61d90379c13b37f4d0d33bd81081c
Jinho Kim <dankalman1234@gmail.com>
2025-10-29 10:15:37 -0700

    sherlock_install.sh

--- scripts/sherlock_scripts/rshuai/sherlock_install.sh
+++ scripts/sherlock_scripts/rshuai/sherlock_install.sh
@@ -1,9 +1,9 @@
 # # Pull apptainer image
 # mkdir -p $GROUP_HOME/containers
-# apptainer pull $GROUP_HOME/containers/pytorch_25.08.sif docker://nvcr.io/nvidia/pytorch:25.08-py3
+# apptainer pull $GROUP_HOME/containers/pytorch_24.12.sif docker://nvcr.io/nvidia/pytorch:24.12-py3
 
 # Set paths
-export IMG="$GROUP_HOME/containers/pytorch_25.08.sif"
+export IMG="$GROUP_HOME/containers/pytorch_24.12.sif"
 export UV_CACHE_DIR="$SCRATCH/cache"
 export REPO_DIR="/home/users/rshuai/code/allatom-design"
 export ENV_DIR="$SCRATCH/envs"
@@ -34,8 +34,19 @@ source allatom_design/bin/activate
 cd "'"$REPO_DIR"'"
 uv pip sync uv_indexes.txt uv.lock --index-strategy=unsafe-best-match --cache-dir '"$UV_CACHE_DIR"'
 uv pip install -e .
-
-# maybe risky but if needed, to install colabdesign
-uv pip install "jax[cuda12]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-uv pip install colabdesign@git+https://github.com/sokrypton/ColabDesign.git@d024c4e846fea83c090afcbe89a313eeee8ec01e
 '
+
+# # Install colabdesign.
+# uv pip install colabdesign@git+https://github.com/sokrypton/ColabDesign.git@d024c4e846fea83c090afcbe89a313eeee8ec01e
+# uv pip install optax==0.1.7
+# uv pip install flax==0.12.0
+# uv pip install "jax[cuda12]==0.4.34" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# uv pip install numpy==1.26.3
+
+# # Install Protpardelle.
+# rm -rf protpardelle-1c  # ensure protpardelle-1c is not already in the repo
+# git clone https://github.com/ProteinDesignLab/protpardelle-1c.git
+# cd protpardelle-1c
+# git checkout 7ebffddcd3b25b986ed36fccebce9676f50ce676
+# cd ..
+# uv pip install -e protpardelle-1c --no-deps
