e3f6247f6da9755a64f4f3fbf4172f78165f380a
Jinho Kim <zhkim216@stanford.edu>
2026-03-23 14:26:36 -0700

    cbrc

--- scripts/sherlock_scripts/jinho/utils/gather_csvs.sh
+++ scripts/sherlock_scripts/jinho/utils/gather_csvs.sh
@@ -4,10 +4,9 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 EXP_DIRS=(
-    /scratch/users/zhkim216/out_dir/eval_ligand_seq_des/eval_exp36_cfg0_rfd3val_af3
-    /scratch/users/zhkim216/out_dir/eval_ligand_seq_des/eval_exp36_cfg1_rfd3val_af3
+    /scratch/users/zhkim216/out_dir/eval_ligand_seq_des/eval_exp37_cfg11_denovoval_seq8_af3_ccd0VI_len150_250_350_450    
 )
 
-OUTPUT_TAR=/scratch/users/zhkim216/out_dir/eval_ligand_seq_des/collected_csvs/exp36_rfd3val.tar.gz
+OUTPUT_TAR=/scratch/users/zhkim216/out_dir/eval_ligand_seq_des/collected_csvs/denovoval_scaffold_generation_remaining_test.tar.gz
 
 python3 "$SCRIPT_DIR/gather_csvs.py" "$OUTPUT_TAR" "${EXP_DIRS[@]}"
